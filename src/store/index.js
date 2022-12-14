588f406633d24eacdc8f457f5dc553cffdad5ac1
soohyun <suhyeon.kan@gmail.com>
2022-12-14 15:14:57 +0900

    Add: 로그인 관리 기능 추가해서 다중 슬라이스 작업 수행해보기

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,14 @@
 
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 //더이상 if문 쓸 필요가 없음
 //redux-toolkit과 createSlice 사용하면 기존 상태값 바뀌지 않음
 //자동으로 원래 있는 상태를 복사한다고 함
 const counterSlice = createSlice({
 	name: 'counter',
-	initialState,
+	initialState: initialCounterState,
 	reducers: {
 		increment(state) {
 			state.counter++;
@@ -58,13 +58,32 @@ const counterSlice = createSlice({
 // 	return state;
 // };
 
+const initialAuthState = {
+	isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+	name: 'authentication',
+	initialState: initialAuthState,
+	reducers: {
+		login(state) {
+			state.isAuthenticated = true;
+		},
+		logout(state) {
+			state.isAuthenticated = false;
+		},
+	},
+});
+
 //모든 리듀서를 하나의 큰 리듀서로 병합
 const store = configureStore({
 	//reducer 여러개 일때 이렇게 사용
 	// reducer: { counter: counterSlice.reducer },
-	reducer: counterSlice.reducer,
+	reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 //액션 객체를 생성하고 고유한 식별자를 생성할 필요 없음
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
